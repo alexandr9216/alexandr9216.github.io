efbbc54292af8e1922167ff27fe3fea6eeca6aa0
Alexandr <alexandrhr92@gmail.com>
2019-03-13 10:02:07 +0600

    qwerty

--- taobao/taobao.js
+++ taobao/taobao.js
@@ -6,7 +6,7 @@
 
     var html_block_result_preview = '' +
         '<div id="block_result_preview">' +
-            '<div class="brp_title"><textarea width="100%"> </textarea></div>'+
+            '<div class="brp_title"><textarea style="width:100%; height: 56px;"></textarea></div>'+
             '<div class="brp_images"></div>'+
             '<div class="brp_price"></div>'+
             '<div class="brp_var_attr"></div>'+
