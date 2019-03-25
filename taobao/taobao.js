7da12ad5c5fb4cf8fdfc5f322e3845b33c9bc24c
Alexandr <alexandrhr92@gmail.com>
2019-03-25 09:48:35 +0600

    qwerty

--- taobao/taobao.js
+++ taobao/taobao.js
@@ -409,7 +409,7 @@ jQuery(document).ready(function($) {
         //кликаем по всем вариантам опций по очередно, чтобы узнать скидку
         var i = -1;
         var length_skuMap = Object.keys(result.skuMap).length;//конвертируем объект в массив
-        var interval_time = 300;//c каким интервалом мс. будем кликать
+        var interval_time = 100;//c каким интервалом мс. будем кликать
         //alert(length_skuMap);
         for (var key in result.skuMap) {
             i++;
